4114d46b7458efb0edf6ba69d3f6dee7a2dfff56
Peter-Michael Osera <osera@cs.grinnell.edu>
2026-01-30 09:30:21 -0600

    project-specific additions

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # Barcodes
 
+## About
+
 _(TODO: insert a description of the project here!)_
 
-## Authors
+### Authors
 
 *   _(TODO: insert your names here!)_
 *  ...
 
-## Resources
+### Resources
 
 *   _(TODO: list your resources here!)_
 *   ...
+
+## Write-up
+
+### Part 2.1: Old Hat, New Hat
+
+_(TODO: fill me in!)_
+
+### Part 2.2: Iceberg, Right Head!
--- src/main/java/edu/grinnell/csc207/exploration/StringExploration.java
+++ src/main/java/edu/grinnell/csc207/exploration/StringExploration.java
@@ -1,6 +1,22 @@
 package edu.grinnell.csc207.exploration;
 
 public class StringExploration {
+
+    /** TODO: fill me in and my docstring! */
+    public static String intersperse(String[] strs) {
+        return null;
+    }
+
+    /** TODO: fill me in and my docstring! */
+    public static String parseName(String fullName) {
+        return fullName;
+    }
+
+    /** TODO: fill me in and my docstring! */
+    public static boolean forgivingPrompt(String question) {
+        return false;
+    }
+
     // N.B., to run this program, use the following maven command to specify
     // this file as the program entry point rather than the class specified in
     // the pom.xml file:
