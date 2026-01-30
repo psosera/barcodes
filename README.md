f753c1e4160f0a6826fad26da18c5944fc288001
Peter-Michael Osera <osera@cs.grinnell.edu>
2026-01-30 09:06:52 -0600

    project starter code

---
+++ README.md
@@ -0,0 +1,13 @@
+# Barcodes
+
+_(TODO: insert a description of the project here!)_
+
+## Authors
+
+*   _(TODO: insert your names here!)_
+*  ...
+
+## Resources
+
+*   _(TODO: list your resources here!)_
+*   ...
---
+++ src/main/java/edu/grinnell/csc207/exploration/StringExploration.java
@@ -0,0 +1,12 @@
+package edu.grinnell.csc207.exploration;
+
+public class StringExploration {
+    // N.B., to run this program, use the following maven command to specify
+    // this file as the program entry point rather than the class specified in
+    // the pom.xml file:
+    //
+    // mvn compile exec:java -q "-Dexec.mainClass=edu.grinnell.csc207.exploration.StringExploration"
+    public static void main(String[] args) {
+        // TODO: write your exploration code here!
+    }
+}
